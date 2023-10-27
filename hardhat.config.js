601648682f977f520df5bf7e8aa6bb9bb1fa957c
jonas-sebera <sebejaz99@gmail.com>
2023-10-27 14:27:10 +0200

    updated hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-};
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.8",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
