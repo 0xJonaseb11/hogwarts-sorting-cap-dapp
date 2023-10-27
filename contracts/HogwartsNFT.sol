402792f7de8767e8963c3ba78c9a13a90ab27153
jonas-sebera <sebejaz99@gmail.com>
2023-10-27 15:14:10 +0200

    JSDoc comments corrected

--- contracts/HogwartsNFT.sol
+++ contracts/HogwartsNFT.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.20;
 
-import { OpenZeppelin } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import { ERC721URIStorage } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract HogwartsNFT is ERC721URIStorage, Ownable {
     
     /**
-    *let's create different mappings
-    *@param first associated uint256 to address with address that made a request
-    *@param second associates address to a uint256 with the index of the hogwarts house they received
-    *@param third associates address to a bool which checks whether an address has already minted or not
-    *@param fourth associates address to a string with the name of the address  
+    * @dev let's create different mappings
+    * first associated uint256 to address with address that made a request
+    * second associates address to a uint256 with the index of the hogwarts house they received
+    * third associates address to a bool which checks whether an address has already minted or not
+    * fourth associates address to a string with the name of the address  
     */
 
     mapping(uint256 => address) public s_requestIdToSender;
@@ -24,9 +24,9 @@ contract HogwartsNFT is ERC721URIStorage, Ownable {
 
     /**
     string  declares an internal array called houseTokenURIs 
-    *@param which contains strings representing the URIs 
-    *@param for the token metadata of the four Hogwarts 
-    *@param houses (Gryffindor, Hufflepuff, Ravenclaw, Slytherin) 
+    * which contains strings representing the URIs 
+    * for the token metadata of the four Hogwarts 
+    * houses (Gryffindor, Hufflepuff, Ravenclaw, Slytherin) 
     */
     string[] internal houseTokenURIs = [
         "ipfs://QmXja2QBKsNW9qnw9kKfcm25rJTomwAVJUrXekYFJnVwbg/Gryffindor.json",
@@ -39,24 +39,24 @@ contract HogwartsNFT is ERC721URIStorage, Ownable {
     event NftMinted(uint256 house, address minter, string name);
 
     /**let's set the initial contract state with 
-    *@param  a constructor with our contract_Name and symbol HP
+    *  a constructor with our contract_Name and symbol HP
     */
-    constructor() ER721("HogwartsNFT", "HP") {
+    constructor() ERC721("HogwartsNFT", "HP") {
         s_tokenCounter = 0;
     }
      
      /**this is a view read only funcn that allows anyone to check whether 
-     *@param a specific address _user has already minted an NFT 
-     *@param by returning a bool indecating minted or not
+     * a specific address _user has already minted an NFT 
+     * by returning a bool indecating minted or not
     */
     function hasMintedNFT(address _user) public view returns(bool) {
         return hasMinted[_user];
     }
     
     /**
-    *@param This func allows anyone to query the index 
-    *@param of the Hogwarts house associated 
-    *@param with a specific address _user  also returns uint256 for index
+    * This func allows anyone to query the index 
+    * of the Hogwarts house associated 
+    * with a specific address _user  also returns uint256 for index
     */
 
     function getHouseIndex(address _user) public view returns(uint256) {
@@ -64,11 +64,11 @@ contract HogwartsNFT is ERC721URIStorage, Ownable {
     }
 
     /**
-    *@param lets allow contract owners to mint NFTs for users
-    *@param and handles some errors, initialises a token, prevents double minting etc
+    * lets allow contract owners to mint NFTs for users
+    * and handles some errors, initialises a token, prevents double minting etc
     */
 
-    function mintNFT(address recipient, uint256 house, string memory name) external onlOwner {
+    function mintNFT(address recipient, uint256 house, string memory name) external onlyOwner {
         // ensure the address has not minted already
         require(!hasMinted[recipient], "You have already minted");
 
@@ -86,12 +86,12 @@ contract HogwartsNFT is ERC721URIStorage, Ownable {
     }
 
     /**
-    *@param This function overrides the _beforeTokenTransfer 
-    *@param function of the parent contract ERC721.
-    *@param it inforces a custom transfer condition where transfers
-    *@param are only allowed from or to the zero address address(0),
-    *@param indeicating that the tokens are soulbound within the hogwarts context
-    *@param when the condition is not met, it reverts the transaction
+    * This function overrides the _beforeTokenTransfer 
+    * function of the parent contract ERC721.
+    * it inforces a custom transfer condition where transfers
+    * are only allowed from or to the zero address address(0),
+    * indeicating that the tokens are soulbound within the hogwarts context
+    * when the condition is not met, it reverts the transaction
     */
 
     function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,37 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
+const vrfCoordinatorV2Address = process.env.VRFaddress;
+const subId = process.env.SubId;
+const keyHash = process.env.keyHash;
+const callbackGasLimit = process.env.gasLimit;
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+    console.log("Deploying Hogwarts NFT Contract...")
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    const HogwartsNFT = await hre.ethers.getContractFactory("HogwartsNFT");
+    const hogwartsNFT = await HogwartsNFT.deploy();
 
-  await lock.waitForDeployment();
+    let currentBlock = await hre.ethers.provider.getBlockNumber();
+    while (currentBlock + 5 > (await hre.ethers.provider.getBlockNumber())) {}
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
-}
+    const hogwartsAddress = await hogwartsNFT.getAddress();
+    console.log("Hogwarts NFT deployed to:", hogwartsAddress);
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.log("Deploying Random House Assignment Contract...")
+
+    const RandomHouse = await hre.ethers.getContractFactory("RandomHouseAssignment");
+    const randomHouse = await RandomHouse.deploy(hogwartsAddress, vrfCoordinatorV2Address, subId, keyHash, callbackGasLimit);
+
+    while (currentBlock + 5 > (await hre.ethers.provider.getBlockNumber())) {}
+
+    const randomAddress = await randomHouse.getAddress();
+    console.log("Random House Assignment deployed to:", randomAddress);
+
+    //Transfering ownership
+    await hogwartsNFT.transferOwnership(randomAddress);
+    console.log("Ownership transferred");
+
+  }
+
+  main();
