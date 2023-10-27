3ab3b6d39c87abaec1a23152a66a94246d2d1e91
jonas-sebera <sebejaz99@gmail.com>
2023-10-27 09:46:24 +0200

    initialize contract with constructor

--- contracts/RandomHouseAssignment.sol
+++ contracts/RandomHouseAssignment.sol
@@ -23,5 +23,27 @@ contract RandomHouseAssignment is VRFConsumerBaseV2 {
     
     mapping(uint256 => address) private s_requestIdToSender;
     mapping(address => string) private s_nameToSender;
+    
+    //Emit event when requestedNft
+    /**
+    *@param it includes the requestId and requester 
+    *@param address as indexed parameters for easy retrieval. 
+    */
+
+    event NftRequested(uint256 indexed requestId, address requester);
+
+    constructor(
+        address _nftContract,
+        address vrfCoordinatorV2Address,
+        uint64 subId,
+        bytes32 keyHash,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2Address) {
+        nftContract = HogwartsNFT(_nftContract);
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2Address);
+        i_subscriptionId = subId;
+        i_keyHash = keyHash;
+        i_callbackGasLimit = callbackGasLimit;
+    }
 
 }
