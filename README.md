d76e854b68694f46e83da147efb89aa8c50eafb5
jonas-sebera <sebejaz99@gmail.com>
2023-10-27 06:44:16 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+### Author: @Jonas-sebera
+
 # hogwarts-sorting-cap-dapp
 Get to build a Sorting Hat dApp and make it choose one of the four Hogwarts houses for you.
+
+Let's go back to our childhood
+
+<hr>
+
+### @Jonas-sebera
---
+++ contracts/HogwartsNFT.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { OpenZeppelin } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
