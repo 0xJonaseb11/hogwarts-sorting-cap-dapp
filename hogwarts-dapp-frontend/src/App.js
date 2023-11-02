07c06b1273bc334d97f72a74200b672f13fb3268
jonas-sebera <sebejaz99@gmail.com>
2023-11-02 06:27:37 +0200

    App init

--- hogwarts-dapp-frontend/src/App.js
+++ hogwarts-dapp-frontend/src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React { useState, useEffect } from "react";
+import useSound from "use-sound";
+import Web3 from "web3";
+import HogwartsNFT from "./artifacts/HogwartsNFT.json";
+import RandomHouseAssignment from "./artifacts/RandomHouseAssignment.json";
+import HogwartsLogo from "./assets/hogwarts_logo.png";
+import Lottie from "lottie-react";
+import HPLoader from "./loaders/hpLoader.json";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+// import App.css
+import "./App.css";
+
+//import audio
+import gryffindorSound from "./sounds/gryffindor.mp3";
+import hufflepuffSound from "./sounds/hufflepuff.mp3";
+import ravenclawSound from "./sounds/ravenclaw.mp3";
+import slytherinSound from "./sounds/slytherin.mp3";
+import thinkingSound from "./sounds/thinking.mp3"; 
+import bgSound from "./sounds/bg_music.mp3";
+
+
+//initialize a web3 library
+const web3 = new Web3(window.ethereum);
+
+
+const App = () => {
+
+  
+  return (
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
